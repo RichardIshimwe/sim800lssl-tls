4281e0d2e7c9fe331059a1008bdce0de2af74879
evening8 <ishimwerichard26@gmail.com>
2023-06-18 15:50:24 +0200

    test sim800 secured request

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+    res.status(200).json({
+        messages: "hello from gsm"
+    })
+})
+
+app.listen(port);
